e3b0e34e002c81d392c628640890633bc0a69ca8
Dave Corley <smokydave@icloud.com>
2025-03-09 00:40:45 -0700

    CLEANUP: Add normalize_path to lib.rs and make directory_node imports public only inside the crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,23 @@ pub mod display_tree;
 pub mod vfs;
 pub mod vfs_file;
 
-pub use directory_node::{DirectoryNode, VFSDirectory};
+pub(crate) use directory_node::{DirectoryNode, VFSDirectory};
 pub use display_tree::{DisplayTree, SerializeType, VFSSerialize};
 pub use vfs::VFS;
 pub use vfs_file::VfsFile;
+
+use std::path::{Path, PathBuf};
+/// Lowercase path and convert path separators to unix-style
+pub fn normalize_path<P: AsRef<Path>>(path: P) -> PathBuf {
+    PathBuf::from(
+        path.as_ref()
+            .to_string_lossy()
+            .to_lowercase()
+            .replace("\\", "/"), // Additional handling for duplicate path separators
+                                 // But probably too expensive to actually use?
+                                 // .split('/')
+                                 // .filter(|s| !s.is_empty())
+                                 // .collect::<Vec<_>>()
+                                 // .join("/"),
+    )
+}
