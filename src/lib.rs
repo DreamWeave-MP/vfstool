f72902a56ef584d5031a72c4178c029a05a49a66
Dave Corley <smokydave@icloud.com>
2025-03-11 17:49:56 -0700

    FEAT: Add helper methods for reading bsa files from archives into the vfs

--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,45 @@ pub fn normalize_path<P: AsRef<Path>>(path: P) -> PathBuf {
                                  // .join("/"),
     )
 }
+
+pub mod archives {
+    use super::{VFS, VfsFile};
+    use bsatoollib::BSAFile;
+    use std::{collections::HashMap, path::PathBuf, sync::Arc};
+
+    pub type ArchiveList = Vec<Arc<BSAFile>>;
+
+    pub fn in_config(config: &openmw_cfg::Ini) -> Vec<&str> {
+        config
+            .general_section()
+            .iter()
+            .filter_map(|(k, v)| match k == "fallback-archive" {
+                false => None,
+                true => Some(v),
+            })
+            .collect()
+    }
+
+    pub fn from_set(file_map: &HashMap<PathBuf, VfsFile>, archive_list: Vec<&str>) -> ArchiveList {
+        archive_list
+            .iter()
+            .filter_map(|archive| file_map.get(&PathBuf::from(archive.to_ascii_lowercase())))
+            .filter_map(|valid_archive| BSAFile::from(valid_archive.path().to_string_lossy()).ok())
+            .map(Arc::new)
+            .collect()
+    }
+
+    pub fn file_map(archives: &ArchiveList) -> HashMap<PathBuf, VfsFile> {
+        archives
+            .iter()
+            .flat_map(|archive| {
+                archive.get_list().into_iter().map(|file_struct| {
+                    (
+                        PathBuf::from(&file_struct.name),
+                        VfsFile::from_archive(&file_struct.name, Arc::clone(archive)),
+                    )
+                })
+            })
+            .collect()
+    }
+}
